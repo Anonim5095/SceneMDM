998441ab2bc533b6f6ed70291906d618f9845eac
Anonim <tes.5063524@gmail.com>
2024-03-15 02:13:35 +0900

    commit

--- prepare/link_mdm.sh
+++
@@ -1,4 +0,0 @@
-ln -s ../motion-diffusion-model/body_models/smpl ./body_models/.
-ln -s ../motion-diffusion-model/glove .
-ln -s ../motion-diffusion-model/t2m .
-ln -s ../motion-diffusion-model/dataset/HumanML3D ./dataset/.
--- run_fit_seq.sh
+++
@@ -1,6 +0,0 @@
-python visualize/joints2smpl/fit_seq.py \
-    --joint_category AMASS_smplx \
-    --num_smplify_iters 20 \
-    --input_path ./save/humanml_only_text_condition/result_a_person_sitting_on_a_bed_lies_down/sample28_rep00.mp4 \
-    # --input_path ./save/humanml_only_text_condition/result_a_person_is_reading_a_book_while_lying_down/sample17_rep00.mp4 \
-    # --input_path ./save/humanml_only_text_condition/result_a_person_is_reading_a_book_while_lying_down/sample17_rep00.mp4 \
